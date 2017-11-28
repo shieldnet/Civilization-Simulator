5378d3d4a950163be16ba5bb79843b36e3d923fc
ATEZ <shieldnet64@gmail.com>
2017-11-28 16:45:48 +0900

    Generate xxMaker(Person) Object in Civilization Obj.

--- src/civilization.py
+++ src/civilization.py
@@ -1,20 +1,33 @@
 import sys
+import Person
 
 # Civilization Class
 class Civilization:
     def __init__(self):
-        self.exchange_table = [[0 for col in range(3)] for row in range(3)]
-        self.rsc_enum = {'food': 0, 'water': 1, 'wood': 2}
-        self._init_table()
+        self.my_exchange_table = [[0 for col in range(3)] for row in range(3)]
+        self.other_exchange_table = [[0 for col in range(3)] for row in range(3)]
         
-        pass
+        self.rsc_enum = {'food': 0, 'water': 1, 'wood': 2}
+        
+        self._init_rsc_table(self.my_exchange_table, -1)
+        self._init_rsc_table(self.other_exchange_table, -1)
+        self.degree_of_civilized = 0
+        # Resource Object List
+        
+        # Person Object List
+        self.tool_maker = Person.ToolMaker()
+        self.food_maker = Person.FoodMaker()
+        self.water_maker = Person.WaterMaker()
+
+        # Communication Object
+        
     
-    def _init_table(self):
-        for r in range(0,3):
-            for c in range(0,3):
+    def _init_rsc_table(self, _table, num):
+        for r in range(0, 3):
+            for c in range(0, 3):
                 if r is c: pass
                 else:
-                    self.exchange_table[r][c] = 1
+                    _table[r][c] = num
     
     def rsc_produce(self):
         pass
@@ -30,4 +43,5 @@ class Civilization:
     
 a = Civilization()
 
-print(a.exchange_table)
+print(a.my_exchange_table)
+print(a.other_exchange_table)
