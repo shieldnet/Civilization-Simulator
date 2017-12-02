39d76baf864df9e12a81b623052edb0752dba90c
Taekyun Lee <puffnjackie@gmail.com>
2017-12-03 04:46:01 +0900

    remove tabs for correcting error

--- src/front.py
+++ src/front.py
@@ -9,6 +9,9 @@ from firebase import firebase
 import plotly
 
 app = dash.Dash()
+
+app.scripts.config.serve_locally = True
+
 firebase = firebase.FirebaseApplication('https://villiagesimulator.firebaseio.com', None)
 vil1 = firebase.get('/Civilization1', None)
 print(vil1)
@@ -20,20 +23,16 @@ print(list(map(int,vil2.values())))
 app.layout = html.Div([
 
     dcc.Interval(id='interval-component',interval=1*1000),
+    dcc.Interval(id='interval-component2',interval=1*1000),
     html.H1(children='Village Simulator'),
 
      html.Div(children='''
          2017 OOP Project4 : Village Simulator
      '''),
-    dcc.Tabs(
-        tabs=[
-            {'label': 'all data', 'value': 1},
-            {'label': 'population_graph', 'value': 2}
-        ],
-        value=1,
-        id='tabs'
-    ),
-    html.Div(id='tab-output')
+
+    dcc.Graph(id = 'live-update-graph'),
+    dcc.Graph(id = 'live-update-graph2')
+
 ], style={
     'width': '80%',
     'fontFamily': 'Sans-Serif',
@@ -42,46 +41,46 @@ app.layout = html.Div([
 })
 
 
-# children=[
-    #
-    # dcc.Interval(id='interval-component',interval=1*1000),
-    #
-    # html.H1(children='Village Simulator'),
-    #
-    # html.Div(children='''
-    #     2017 OOP Project4 : Village Simulator
-    # '''),
-    # dcc.Graph(id='live-update-graph')
-
-
-
-
-
-
-@app.callback(Output('tab-output', 'children'),[Input('tabs', 'value')],
-              events=[Event('interval-component', 'interval')])
-def update_alldata_server(value):
+@app.callback(Output('live-update-graph', 'figure'),events=[Event('interval-component', 'interval')])
+def update_alldata_server2():
     vil1 = firebase.get('/Civilization1', None)
-
     vil2 = firebase.get('/Civilization2', None)
-    fig = plotly.tools.make_subplots(rows=6, cols=6, vertical_spacing=1.0)
-    fig={
-        'data': [
-            {'x': list(vil1.keys()), 'y': list(map(int, vil1.values())), 'type': 'bar', 'name': 'Village1'},
-            {'x': list(vil1.keys()), 'y': list(map(int, vil2.values())), 'type': 'bar', 'name': 'Village2'},
-            ],
+    figure = {'data': [
+        {'x': list(vil1.keys()), 'y': list(map(int, vil1.values())), 'type': 'bar', 'name': 'Village1'},
+        {'x': list(vil1.keys()), 'y': list(map(int, vil2.values())), 'type': 'bar', 'name': 'Village2'},
+
+    ],
+
         'layout': {
             'title': 'All data on Village1,2'
-            },
+        },
         'legend': {'x': 0, 'y': 1}
     }
+    return figure
 
-    return html.Div([
-        dcc.Graph(
-            id='graph',
-            figure = fig
-        )
-        ])
+
+@app.callback(Output('live-update-graph2', 'figure'),events=[Event('interval-component2', 'interval')])
+def update_alldata_server():
+    vil1 = firebase.get('/Civilization1', None)
+    vil2 = firebase.get('/Civilization2', None)
+    figure2 = {'data': [
+        {'labels': ['Civilization1-people', 'Civilization2-people'], 'values': [vil1['Civil1_NumPeople'],vil2['Civil2_NumPeople']], 'type': 'pie', 'name': 'Village'},
+
+    ],
+
+        'layout': {
+            'title': 'population data on Village1,2'
+        },
+        'legend': dict(
+         font=dict(color='#CCCCCC', size='10'),
+         orientation='h',
+         bgcolor='rgba(0,0,0,0)')
+    }
+
+    return figure2
+
+
+
 
 
 if __name__ == '__main__':
